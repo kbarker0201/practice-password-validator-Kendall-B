d5025f1f7dc83c52653aabc93cb945d87f5be3b3
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 18:19:59 -0500

    updates

--- password-val.js
+++ password-val.js
@@ -1,1 +1,23 @@
-console.log();
+const readline = require('readline-sync');
+
+let password;
+let isUpperCase = false;
+let isLowerCase = false;
+
+do {
+password = readline.question("Please enter a password: ");
+password.split("");
+for (const letter of password) {
+    if (letter.toUpperCase() === true) {
+        isUpperCase === true;
+    } else if (letter.toLowerCase === true) {
+        isLowerCase === true;
+    } else {isUpperCase === false;
+        isLowerCase === false;
+        console.log("Password requires at least one uppercase and one lowercase letter.");
+    }
+}}
+while (password.length <= 8 || isUpperCase != 
+true || isLowerCase != true);
+
+console.log("Your password has been saved.");
